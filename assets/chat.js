1abace0643762f68e2da617a72571bd6219bc916
Akshit Arora <akshitarora4731@gmail.com>
2020-03-04 02:27:41 +0530

    deployment fix

--- assets/chat.js
+++ assets/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect("http://localhost:5000");
+var socket = io.connect("http://localhost:5000"||"https://chat-app--007.herokuapp.com/");
 
 var username = document.getElementById("username");
 var message = document.getElementById("message");
