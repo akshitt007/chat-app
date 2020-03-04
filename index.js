e1960e1a5c1fda34db24d5da59199ce072657325
Akshit Arora <akshitarora4731@gmail.com>
2020-03-04 16:40:37 +0530

    fix

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var port = process.env.PORT || 8000;
-var socket = require('socket.io');
+var io = require('socket.io');
 var app = express();
 const path = require('path');
-//app.set ('views',path.join(__dirname, 'views'));
+
 app.set ('view engine','ejs');
 app.set ('views',path.join(__dirname, 'views'));
 app.use(express.static('assets'));
 const chatServer = require('http').Server(app);
-var chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
-chatServer.listen(5000);
-console.log('chat server is listening on port 5000');
+var chatSockets = require('./config/chat_sockets').chatSockets(chatServer||process.env.PORT);
+chatServer.listen(process.env.PORT||5000);
+console.log('chat server is listening on port 5000 or by heroku');
 
+//io.configure(function () { 
+ //   io.set("transports", ["xhr-polling"]); 
+   // io.set("polling duration", 10); 
+  //});
 
 app.get('/',function(req,res){
     res.render('home')
